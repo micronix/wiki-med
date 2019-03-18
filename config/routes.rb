f9052b25799288ec7a6b60c2ef5007a917d545f6
Tony Wade <wadant3@gmail.com>
2019-03-18 20:36:44 +0000

    Upgrade to 5.1.6 rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :facilities
   resources :procedures
@@ -64,4 +65,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end
